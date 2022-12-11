95e470429c167a36f0dce62b6ace394a3f9b5f6a
jpinedadev <jpinedadeveloper@gmail.com>
2022-12-11 18:22:33 -0500

    js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
-localStorage.setItem('001', 47.99);
-localStorage.setItem('002', 47.99);
+//localStorage.setItem('001', 47.99);
+//localStorage.setItem('002', 47.99);
 //alert( localStorage.getItem('test') ); // 1
 //delete localStorage.test;
 
 
+function myFunction(id,price){
+
+    //obteniendo datos
+    console.log(id+' '+price)
+    let obtener = document.getElementById(id)
+    console.log(obtener)
+    let nombre = obtener.querySelector(".nombre").textContent;
+    console.log(nombre)
+    let precio = obtener.querySelector(".precio").textContent;
+    console.log(precio)
+
+    //guardando en local storage
+    localStorage.setItem(`${id}`, `${price},${nombre}`);
+    //obtener local storage
+    console.log(localStorage.getItem(`${id}`));
+    console.log(localStorage.getItem(`${id}[0]`))
+}
 
